634fa843dacc7782a4474b66449fa358214bfc40
hoz-map <noir0315@g.ucla.edu>
2021-04-29 13:35:33 +0900

    Week4 maproom

---
+++ Week4/js/map.js
@@ -0,0 +1,80 @@
+// Global variables
+let map;
+let lat = 0;
+let lon = 0;
+let zl = 3;
+// path to csv data
+let toppath = "data/DH151_waste_Top 10.csv";
+let bottompath = "data/DH151_waste_Bottom 10.csv";
+
+// global variables
+let topmarkers = L.featureGroup();
+let bottommarkers = L.featureGroup();
+
+// create the map
+function createMap(lat,lon,zl){
+	map = L.map('map').setView([lat,lon], zl);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+// initialize
+$( document ).ready(function() {
+	createMap(lat,lon,zl);
+
+	// readCSV now accepts 3 arguments
+	// call it twice, once for each layer
+    readCSV(toppath,topmarkers,'red');
+    readCSV(bottompath,bottommarkers,'green');
+});
+
+// function to read csv data
+// this now accepts arguments for path, the featuregroup variable, and the desired color for the circle
+function readCSV(path,featuregroup,color){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			console.log(data);
+			
+			// map the data
+			// need to feed this 3 arguments as well
+			mapCSV(data,featuregroup,color);
+
+		}
+	});
+}
+
+// this has also been modified to accept three arguments
+function mapCSV(data,featuregroup,color){
+	
+    let circleOptions = {
+        radius: 5,
+        weight: 1,
+        color: 'white',
+        fillColor: color, // use the argument for color here
+        fillOpacity: .5,
+    }
+
+	// loop through each entry
+	data.data.forEach(function(item,index){
+		// create marker (use Latitude instead of lat bc the console said Lantitude instead of lat)
+		let topmarker = L.circleMarker([item.Latitude,item.Longitude], circleOptions)
+        .on('mouseover', function(){
+        		this.bindPopup(`<b>${item.Country}</b><p>${item.Waste_generation_rate_kg_person_day}kg/person/day</p>`).openPopup()
+		})
+
+		// add marker to featuregroup
+		// featuregroup is now an argument, so no need to hard code it here
+		featuregroup.addLayer(topmarker)
+
+	})
+
+	// add featuregroup to map
+	featuregroup.addTo(map)
+
+	// fit markers to map
+	map.fitBounds(featuregroup.getBounds())
+}
